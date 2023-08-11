ff692175f2be16901f3396f64c35b17662e19c71
Ostrovskiy32 <osttov@mail.ru>
2023-08-11 23:40:39 +0300

    ch

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,28 @@
+package app.config;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.security.DigestException;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+    @Bean(name = "dog")
+    public Dog getDog() {
+        Dog dog = new Dog();
+        dog.toString();
+        return dog;
+    }
+    @Bean(name = "time")
+    public Timer getTimer() {
+        Timer timer = new Timer();
+        timer.getTime();
+        return timer;
+    }
+
+}
---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,9 @@
+package app.model;
+
+public class Dog extends Animal {
+
+    @Override
+    public String toString() {
+        return "I'm a dog";
+    }
+}
